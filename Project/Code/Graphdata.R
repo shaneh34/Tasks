1627c0b738fe98a5ea563899583dd994135427f4
shaneh34 <sch0001@mix.wvu.edu>
2020-04-10 00:22:47 -0400

    <Shane Harless Project Paper>

--- Project/Code/Graphdata.R
+++ Project/Code/Graphdata.R
@@ -9,7 +9,15 @@ plot (PercentWolfDNA, Heterozygosity, xlab="Percent of Wolf DNA", ylab="Coyote H
 abline(lm(Heterozygosity~PercentWolfDNA), col="red")
 Regression <- lm(Heterozygosity~PercentWolfDNA)
 Regression
-
+Correlationtest <- cor.test(PercentWolfDNA, Heterozygosity, 
+                method = "pearson")
+Correlationtest
+install.packages("ggpubr")
+library("ggpubr")
+ggscatter(Graphdata, x = "WOLF.DNA", y = "RELATIVE.FST", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Percent Wolf DNA", ylab = "Relative Heterozygosity")
 #To do list
 #1 Run a regression analysis to look for correlation.
 #2 Create a phylogeny tree of the coyotes with the data.
